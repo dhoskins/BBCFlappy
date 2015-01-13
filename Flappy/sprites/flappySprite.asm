e974926748150c5601e079b93d704b16b433b0f2
Douglas Hoskins <douglas@hosk.co>
2015-01-13 08:28:00 +0000

    Fixed sprite painting

--- Flappy/sprites/flappySprite.asm
+++ Flappy/sprites/flappySprite.asm
@@ -1,10 +1,20 @@
 .flappySprite
-	EQUB &6,&4
+	EQUB &7,&4
 
 	EQUB &3,&3,&3,&3,&3,&3,&3
-	EQUB &3,&C,&C,&C,&C,&C,&3
-	EQUB &3,&C,&C,&C,&C,&C,&3
+	EQUB &3,&30,&30,&30,&30,&30,&3
+	EQUB &3,&30,&30,&30,&30,&30,&3
 	EQUB &3,&3,&3,&3,&3,&3,&3
+
+\	EQUB &33,&30
+\	EQUB &0A,&0F
+\	EQUB &0F,&0A
+\	EQUB &30,&33
+
+\	EQUB &3,&30,&3,&30
+\	EQUB &30,&3,&30,&3
+\	EQUB &3,&30,&3,&30
+\	EQUB &30,&3,&30,&3
 
 .flappySpriteEnd
 SKIP 6*4
