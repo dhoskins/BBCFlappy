66af54cb9b1abe30be5672176810f68d92b03ad1
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2015-01-14 08:41:41 +0000

    Try unpainting sprites with previous background.
    Currently there is an issue with the stashed background being painted in the wrong place

--- Flappy/paintSprites.asm
+++ Flappy/paintSprites.asm
@@ -1,3 +1,12 @@
+.paintSpritesOn
+	LDA #0
+	STA shouldRestoreStash
+	JMP paintSprites
+
+.paintSpritesOff
+	LDA #1
+	STA shouldRestoreStash
+
 .paintSprites
 	LDA #50
 	STA xcoord
@@ -6,8 +15,13 @@
 
 	LDA #(flappySprite MOD 256)
 	STA sprite
-
 	LDA #(flappySprite DIV 256)
 	STA sprite+1
+
+	LDA #(flappySpriteEnd MOD 256)
+	STA spriteStash
+	LDA #(flappySpriteEnd DIV 256)
+	STA spriteStash+1
+
 	JSR paintSprite
 RTS
